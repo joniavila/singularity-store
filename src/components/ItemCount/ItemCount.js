f71cade31b70b0e78f2e5b8a4224c94b0279097f
Jonas <jonasavila27@gmail.com>
2021-06-07 20:06:40 -0300

    agregado de boton y modificacion de hooks

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -6,9 +6,8 @@ import FormControl from 'react-bootstrap/FormControl'
 import Toast from 'react-bootstrap/Toast'
 import Card from 'react-bootstrap/Card'
 
-const ItemCount = (props) => {
-        const stock = props.stock
-        const [contador , setContador] = useState(0)
+const ItemCount = ({stock,initial,onAdd}) => {
+        const [contador , setContador] = useState(parseInt(initial))
         const [show, setShow] = useState(false);
         const [bandera,setBandera] = useState(false)
         const sumar = () =>{
@@ -34,16 +33,19 @@ const ItemCount = (props) => {
                 <InputGroup className="mb-3">
                     <InputGroup.Prepend>
                         <Button onClick={sumar} disabled={bandera}>
-                            AGREGAR
+                            +
                         </Button>
                     </InputGroup.Prepend>
                     <FormControl aria-describedby="basic-addon1" value={contador} readOnly></FormControl>
                     <InputGroup.Append>
                         <Button onClick={restar} disabled={contador===0}>
-                            DESAGREGAR
+                            -
                         </Button>
                     </InputGroup.Append>
                 </InputGroup>
+                <Button onClick={onAdd} disabled={contador===0}>
+                        AGREGAR
+                </Button>
                 </Card>
                 <Toast show={show} onClose={() => setShow(false)} delay={3000} autohide>
                     <Toast.Header>ERROR</Toast.Header>
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 import ItemCount from '../ItemCount/ItemCount'
 
 class ItemListContainer extends Component {
+    onAdd = () => {
+        console.log('se agrego el producto')
+        alert('Se agrego el producto al carrito')
+    }
     render() {
         return (
             <div>
                 {this.props.name}
-                <ItemCount stock="5"/>
+                <ItemCount stock="5" initial='0' onAdd={this.onAdd}/>
             </div>
         );
     }
