d5d5877cd1e01df1ed470c1602f6424075200337
Jonas <jonasavila27@gmail.com>
2021-07-30 18:46:05 -0300

    agregado de campos en cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,13 @@
 import React, { useEffect,useState,useContext } from 'react';
 import CartContext from '../Context/CartContext';
-import { Table } from 'react-bootstrap';
 import Toast from 'react-bootstrap/Toast'
 import DeleteForeverOutlinedIcon from '@material-ui/icons/DeleteForeverOutlined';
 import {Link}  from 'react-router-dom';
 import { getFirestore } from '../../firebase';
 import firebase from 'firebase';
 import LinearProgress from '@material-ui/core/LinearProgress';
-import { Button } from 'react-bootstrap';
+import { Button,Table,InputGroup,FormControl } from 'react-bootstrap';
+import './Cart.css'
 
 const Cart = () => {
     const [productos,setProductos] = useState([])
@@ -15,6 +15,11 @@ const Cart = () => {
     const [loading, setLoading] = useState(false)
     const [show, setShow] = useState(false);
     const [idOrden, setIdOrden] = useState(null)
+    const [mail,setMail] = useState('')
+    const [nombre , setNombre] = useState('')
+    const [telefono, setTelefono] = useState('')
+    const [showError, setShowError ] = useState(false)
+
     useEffect(()=>{
         let productosEnCache = []
         productosEnCache = getAll()
@@ -26,15 +31,16 @@ const Cart = () => {
         removeItem(item)
     }
     const confirmarOrden = ()=>{
+        if(mail && nombre && telefono){
         setLoading(true)
         crearCart().then((res)=> {
                 const db = getFirestore()
                 const order = db.collection('orders')
                 const newOrder = {
                     buyer: {
-                      email: 'jonasavila@gmail.com',
-                      name: 'Jonas Avila',
-                      phone: '+3512312312'
+                      email: mail,
+                      name: nombre,
+                      phone: telefono
                     },
                     date: firebase.firestore.Timestamp.fromDate(new Date()),
                     items: res.items,
@@ -45,6 +51,9 @@ const Cart = () => {
                     setIdOrden(id)
                     setLoading(false)
                     setShow(true)
+                    setNombre('')
+                    setMail('')
+                    setTelefono('')
                     // limpia el cart para poder generar uno nuevo
                     clearCache()
                     console.log('Orden cargada con exito: ID' ,id)
@@ -52,6 +61,9 @@ const Cart = () => {
                     console.log(error)
                 })
             })
+        }else{
+            setShowError(true)
+        }
     }
 
     // crea el array con los productos cargados en el carrito y calcula el total
@@ -75,35 +87,58 @@ const Cart = () => {
         return totalAux
     }
     return (
-        <div>
-        <Table variant="dark">
-            <thead>
-                <tr>
-                <th>ID</th>
-                <th>TITULO</th>
-                <th>CATEGORIA</th>
-                <th>PRECIO</th>
-                <th>CANTIDAD</th>
-                <th>ACCIONES</th>
-                </tr>
-            </thead>
-            <tbody>
-                { productos.length ? productos.map((item) => {
-                return <tr key={item.id}> 
-                <td key={item.id}>{item.id}</td>
-                <td key={item.id}>{item.title}</td>
-                <td key={item.id}>{item.category}</td>
-                <td key={item.id}>{item.price}</td>
-                <td key={item.id}>{item.cantidad}</td>
-                <td key={item.id}> <Button variant='dark' onClick={()=>{eliminarProducto(item)}}><DeleteForeverOutlinedIcon style={{color:'red'}}/></Button></td>
-                </tr>
-                })
-               : <div> 
-                   NO HAY PRODUCTOS AGREGADOS EN EL CARRITO. 
-                   PUEDE AGREGAR UNO <Link to='/'>DESDE AQUI</Link>
-                </div>}
-            </tbody>
-        </Table>
+        <div style={{display: '', margin:'20px' }}>
+            <div>
+                <h3> DATOS DEL USUARIO:</h3>
+                <InputGroup>
+                    <InputGroup.Text>Mail</InputGroup.Text>
+                    <FormControl onChange={e => setMail({ val: e.target.value })}/>
+                </InputGroup>
+                <InputGroup>
+                    <InputGroup.Text>Nombre Completo</InputGroup.Text>
+                    <FormControl onChange={e => setNombre({ val: e.target.value })}/>
+                </InputGroup>
+                <InputGroup>
+                    <InputGroup.Text>Telefono/Celular</InputGroup.Text>
+                    <FormControl onChange={e => setTelefono({ val: e.target.value })}/>
+                </InputGroup>
+
+                <Toast show={showError} onClose={() => setShowError(false)} delay={30000} autohide style={{marginLeft:'35%',width:'auto'}}>
+                    <Toast.Header style={{backgroundColor:'red'}}>ERROR</Toast.Header>
+                    <Toast.Body> DEBE INGRESAR MAIL,NOMBRE Y TELEFONO PARA REGISTRAR SU PEDIDO</Toast.Body>
+                </Toast>
+            </div>
+            <div>
+            <h3> PRODUCTOS:</h3>
+                <Table variant="dark">
+                    <thead>
+                        <tr>
+                        <th>ID</th>
+                        <th>TITULO</th>
+                        <th>CATEGORIA</th>
+                        <th>PRECIO</th>
+                        <th>CANTIDAD</th>
+                        <th>ACCIONES</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { productos.length ? productos.map((item) => {
+                            return <tr key={item.id}> 
+                        <td key={item.id}>{item.id}</td>
+                        <td key={item.id}>{item.title}</td>
+                        <td key={item.id}>{item.category}</td>
+                        <td key={item.id}>{item.price}</td>
+                        <td key={item.id}>{item.cantidad}</td>
+                        <td key={item.id}> <Button variant='dark' onClick={()=>{eliminarProducto(item)}}><DeleteForeverOutlinedIcon style={{color:'red'}}/></Button></td>
+                        </tr>
+                        })
+                        : <div> 
+                        NO HAY PRODUCTOS AGREGADOS EN EL CARRITO. 
+                        PUEDE AGREGAR UNO <Link to='/'>DESDE AQUI</Link>
+                        </div>}
+                    </tbody>
+                </Table>
+            </div>
         <Button disabled={productos.length===0} onClick={()=>{confirmarOrden()}}>CONFIRMAR ORDEN</Button>
         { loading ? 
             <LinearProgress color="secondary" />
--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -3,6 +3,7 @@ import ItemCount from '../ItemCount/ItemCount';
 import { Button, Image } from 'react-bootstrap';
 import CartContext from '../Context/CartContext';
 import {Link}  from 'react-router-dom';
+import './ItemDetail.css';
 
 const ItemDetail = ({item}) => {
     const [evento, setEvento] = useState(true)
@@ -13,18 +14,16 @@ const ItemDetail = ({item}) => {
     }
     return (
         <div style={{display: 'flex', margin:'20px' }}>
-        <div>
-            <Image src={`${process.env.PUBLIC_URL}/${item.pictureUrl}`} thumbnail fluid />
-                
-        </div>
-            <div style={{height:'100%',display:'' }} bg=''>
+            <div style={{width:'60%', height:'100%'}}>
+            <Image src={`${process.env.PUBLIC_URL}/${item.pictureUrl}`} rounded />
+            </div>
+            <div style={{height:'100%',marginLeft:'15px',width:'40%'}} bg=''>
                     <h1>{item.title}</h1>
                     <ul>
                     {item.caracteristicas.split('|').map(e => {
                         return <li style={{textAlign: 'initial'}}>{e}</li>
                     })}
                     </ul>
-                    {/* <p>{item.caracteristicas}</p> */}
                     <h3>PRECIO: {item.price}</h3>
                     <div style={{margin: '30px', textAlignLast: 'center'}}>
                         { evento ? (<ItemCount  stock={item.stock} initial='0' onAdd={onAdd} />) :
